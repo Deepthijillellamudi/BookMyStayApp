8009edf5e12bb4adbda8612b121ca6b8c1c8b190
deepthijillellamudi <dj7893@srmist.edu.in>
2026-03-17 14:59:29 +0530

    UC2:Basic Room Types & Static Availability

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+abstract class Room {
+    String type;
+    int beds;
+    double price;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+}
+
+public class BookMyStay {
+    public static void main(String[] args) {
+
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        r1.display();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        r2.display();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        r3.display();
+        System.out.println("Available: " + suiteAvailable);
+    }
 }
